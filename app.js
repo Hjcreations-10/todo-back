c3c58e9af55c2e31788bb0f658e5aa85933fb664
HJ creations <jyothsnasinguluri@gmail.com>
2025-05-13 13:39:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,133 +3,90 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from "cors";
 import tasks from "./models/tasks.js";
+
 const app = express();
 
-app.use(bodyParser.json())
-app.use(express.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
 
-// to POST data
-app.post('/api/addtask', (req, res, next)=>{
-    console.log(req.body)
- const {task,status,deadline} = req.body;
- 
- const tas = new tasks({
-    task,
-    status,
-    deadline
- })
+// POST data
+app.post('/api/addtask', async (req, res) => {
+  const { task, status, deadline } = req.body;
 
- tas.save()
- return res.status(200).json({message: "success"})
-})
+  const tas = new tasks({ task, status, deadline });
 
+  try {
+    await tas.save();
+    return res.status(200).json({ message: "success" });
+  } catch (err) {
+    return res.status(500).json({ message: "Error saving task." });
+  }
+});
 
-
-
-// GET data API
-// app.get('/api/getTask',async (req, res, next)=>{
-//     let tas;
-    
-//         tas = await tasks.find();
-    
-//     if(!tas){
-//         return res.status(404).json({message: "No task Found."})
-//     }
-//     return res.status(200).json({tas})
-// })
-/*app.get('/api/getTask', async (req, res, next) => {
-    let tas;
-    try {
-        tas = await tasks.find();
-    } catch (err) {
-        return res.status(500).json({ message: "Error fetching tasks." });
-    }
-
+// GET data
+app.get('/api/getTask', async (req, res) => {
+  try {
+    const tas = await tasks.find();
     if (!tas || tas.length === 0) {
-        return res.status(404).json({ message: "No task Found." });
+      return res.status(404).json({ message: "No task Found." });
     }
     return res.status(200).json({ tas });
-});*/
+  } catch (err) {
+    return res.status(500).json({ message: "Error fetching tasks." });
+  }
+});
 
+// DELETE task
+app.delete('/api/deletetask/:_id', async (req, res) => {
+  const id = req.params._id;
 
-// delete student
-app.delete('/api/deletetask/:_id',async (req, res, next)=>{
-
-    const id=req.params._id
-
-console.log
-
-    let task_delete;
-    try{
-        task_delete= await tasks.findByIdAndDelete({_id:id})
+  try {
+    const task_delete = await tasks.findByIdAndDelete({ _id: id });
+    if (!task_delete) {
+      return res.status(400).json({ message: "Unable to delete." });
     }
-    catch(err)
-    {
-        return console.log(err)
+    return res.status(200).json({ message: "Deleted." });
+  } catch (err) {
+    return res.status(500).json({ message: "Error deleting task." });
+  }
+});
+
+// GET single task by ID
+app.get('/api/get_task_data/:id', async (req, res) => {
+  const _id = req.params.id;
+
+  try {
+    const task_data = await tasks.findById({ _id });
+    if (!task_data) {
+      return res.status(400).json({ message: "No task Found." });
     }
-    
-    if(!task_delete){
-        return res.status(400).json({message: "unable to delete."})
+    return res.status(200).json({ task_data });
+  } catch (err) {
+    return res.status(500).json({ message: "Error fetching task." });
+  }
+});
+
+// PUT update task
+app.put('/api/edit_task/:id', async (req, res) => {
+  const taskid = req.params.id;
+  const { task, status, deadline } = req.body;
+
+  try {
+    const tsk = await tasks.findByIdAndUpdate(taskid, { task, status, deadline }, { new: true });
+    if (!tsk) {
+      return res.status(400).json({ message: "Unable to update the task." });
     }
+    return res.status(200).json({ tsk });
+  } catch (err) {
+    return res.status(500).json({ message: "Error updating task." });
+  }
+});
 
-    return res.status(200).json({message: "deleted."})
+// MongoDB connection
+mongoose
+  .connect("mongodb+srv://jyothsnasinguluri:Jyothsna@cluster0.0rb62uo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("DB connection error:", err));
 
-})
-
-//edit task data
-app.get('/api/get_task_data/:id', async (req, res, next)=>{
-    const _id = req.params.id
-    let task_data;
-    try{
-        task_data = await tasks.findById({_id});
-    }catch(err){
-        return console.log(err)
-    }
-    if(!task_data){
-        return res.status(400).json({message:"No task Found."})
-    }
-    return res.status(201).json({task_data})
-})
-
-
-
-// update form by id
-app.put('/api/edit_task/:id', async (req, res, next)=>{
-    const taskid = req.params.id
-    const {task, status, deadline} = req.body;
-    let tsk;
-    try{
-        tsk = await student.findByIdAndUpdate(taskid,{
-            task,
-            status,
-            deadline
-        });
-    }catch(err){
-        return console.log(err)
-    }
-    if(!tsk){
-        return res.status(400).json({message:"Unable to update the task."})
-    }
-    return res.status(200).json({tsk})
-})
-// end edit user
-
-//database connection
-
-
-
-mongoose.connect('mongodb+srv://jyothsnasinguluri:Jyothsna@cluster0.0rb62uo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-.then(() => app.listen(4000))
-.then(() =>
-console.log("Connected to Database & Listining to localhost 4000")
-)
-.catch((err) => console.log(err));
-
-export default app;// important for vercel
-
-
-
-
-
-
+export default app; // 👈 important for Vercel
